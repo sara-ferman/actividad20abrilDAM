69874a1eb4a28f1158a294812daa101a220093bb
miriamsanviator <80282014+miriamsanviator@users.noreply.github.com>
2021-05-03 20:41:10 +0200

    Refactorizacion de la clase Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,53 +1,60 @@
 import java.util.Scanner;
 
 public class Calculadora {
-
 	public static void main(String [] args) {
-		Scanner sc = new Scanner(System.in);
-		
+		repetirOperacion();
+	}
+	
+	public static void repetirOperacion() {
+		Scanner scanner = new Scanner(System.in);
 		String respuesta;
-		int menu;
-		float n1;
-		float n2;
+		float numero1 = 0;
+		float numero2 = 0;
 		
 		do {
 			System.out.println("Introduzca el primer numero:");
-			n1 = sc.nextFloat();
+			numero1 = scanner.nextFloat();
 			
 			System.out.println("Introduzca el segundo numero:");
-			n2 = sc.nextFloat();
+			numero2 = scanner.nextFloat();
 			
-			System.out.println("MENU DE OPERACIONES");
-			System.out.println("1. Suma");
-			System.out.println("2. Resta");
-			System.out.println("3. Multiplicacion");
-			System.out.println("4. Division");
-			menu = sc.nextInt();
-			
-			if(menu==1) {
-				float suma = n1 + n2;
-				System.out.println(n1+ " + " + n2+" = "+suma);
-				
-			} else if(menu==2) {
-				float resta = n1 - n2;
-				System.out.println(n1+ " - " + n2+" = "+resta);
-				
-			} else if(menu==3) {
-				float multi = n1 * n2;
-				System.out.println(n1+ " * " + n2+" = "+multi);
-				
-			}else if(menu==4) {
-				float divi = n1 / n2;
-				System.out.println(n1+ " / " + n2+" = "+divi);
-				
-			} else {
-				System.out.println("Error");
-			}
+			menuOperaciones(numero1, numero2);
 			
 			System.out.println("Desea realizar otra operacion?");
-			respuesta = sc.next();
+			respuesta = scanner.next();
 			
 		} while(respuesta.equalsIgnoreCase("s"));
+	}
+	
+	public static void menuOperaciones(float numero1, float numero2) {
+		Scanner scanner = new Scanner(System.in);
+		int eleccionMenu = 0;
+		
+		System.out.println("MENU DE OPERACIONES");
+		System.out.println("1. Suma");
+		System.out.println("2. Resta");
+		System.out.println("3. Multiplicacion");
+		System.out.println("4. Division");
+		eleccionMenu = scanner.nextInt();
+		
+		if(eleccionMenu==1) {
+			float suma = numero1 + numero2;
+			System.out.println(numero1+ " + " + numero2+" = "+suma);
 			
+		} else if(eleccionMenu==2) {
+			float resta = numero1 - numero2;
+			System.out.println(numero1+ " - " + numero2+" = "+resta);
+			
+		} else if(eleccionMenu==3) {
+			float multiplicacion = numero1 * numero2;
+			System.out.println(numero1+ " * " + numero2+" = "+multiplicacion);
+			
+		}else if(eleccionMenu==4) {
+			float division = numero1 / numero2;
+			System.out.println(numero1+ " / " + numero2+" = "+division);
+			
+		} else {
+			System.out.println("Error");
+		}
 	}
 }
